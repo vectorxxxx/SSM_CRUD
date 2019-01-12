8978e98082ce9c33e2e2ed1511edac39e7321a3d
uxiahnan <uxiahnan@qq.com>
2019-01-12 20:07:55 +0800

    mybatis generator逆向工程测试

---
+++ src/test/java/xyz/funnyboy/crud/dao/MBGTest.java
@@ -0,0 +1,35 @@
+package xyz.funnyboy.crud.dao;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Uxiahnan OR 14027
+ * @version Dragon1.0
+ * @createTime 2019年01月12日20时03分
+ * @desciption This is a program.
+ * @since Java10
+ */
+public class MBGTest {
+
+    public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
