6b3013a5bc02bec882fe7f74b6051d6bfb14cd88
uxiahnan <uxiahnan@qq.com>
2019-01-13 20:29:26 +0800

    sqlSession批量操作配置

--- src/test/java/xyz/funnyboy/crud/MapperTest.java
+++ src/test/java/xyz/funnyboy/crud/MapperTest.java
@@ -1,11 +1,16 @@
 package xyz.funnyboy.crud;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import xyz.funnyboy.crud.dao.DepartmentMapper;
+import xyz.funnyboy.crud.dao.EmployeeMapper;
+import xyz.funnyboy.crud.model.Department;
+import xyz.funnyboy.crud.model.Employee;
 
 /**
  * @author Uxiahnan OR 14027
@@ -19,8 +24,18 @@ import xyz.funnyboy.crud.dao.DepartmentMapper;
 public class MapperTest {
     @Autowired
     private DepartmentMapper departmentMapper;
+    @Autowired
+    EmployeeMapper employeeMapper;
     @Test
     public void testCRUD(){
-        System.out.println(departmentMapper);
+//        System.out.println(departmentMapper);
+//        departmentMapper.insertSelective(new Department(null, "研发部"));
+//        departmentMapper.insertSelective(new Department(null, "技术部"));
+
+//        employeeMapper.insertSelective(new Employee(null, "刘备", "男", "liubei@qq.com", 1));
+
+        for(int i=0;i<1000;i++) {
+            
+        }
     }
 }
